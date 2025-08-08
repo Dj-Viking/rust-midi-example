a30eec680bd9b7c339435b29f6d1d2cfe8ffef5d
Dj-Viking <anders.swedishviking@gmail.com>
2025-08-08 08:52:53 -0400

    add ableton input support

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,30 @@ use std::io;
 use std::time::Duration;
 use std::os::raw::c_int;
 
+// takes midi messages outputted from wine Ableton midi track out
+// PipeWire-RT-Event for MIDI to routing out of ableton
+fn get_wine_alsa_input_id(pm: &pm::PortMidi) -> c_int {
+	let mut ret = 0;
+
+	for d in pm.devices().unwrap() {
+		if d.name().contains("WINE ALSA Output #1") {
+			ret = d.id();
+		}
+	}
+
+	ret
+}
+fn get_midithroughport_id(pm: &pm::PortMidi) -> c_int {
+	let mut ret = 0;
+
+	for d in pm.devices().unwrap() {
+		if d.name().contains("Through Port") {
+			ret = d.id();
+		}
+	}
+
+	ret
+}
 fn get_xonek2_id(pm: &pm::PortMidi) -> c_int {
 	let mut ret = 0;
     for d in pm.devices().unwrap() {
@@ -35,15 +59,34 @@ fn handle_midi_msg(m: MyMidiMessage) -> () {
 	println!("{:?}", m);
 }
 
+
+
 fn main() {
+
+	if std::env::args().skip(1).any(|a| a == "help") {
+		println!("[HELP]: type 'cargo run list' to see all available midi devices on this computer");
+		println!("[HELP]: type 'cargo run ableton' to use the midi output from the wine server running ableton");
+		std::process::exit(0);
+	}
+
+	if std::env::args().skip(1).any(|a| a == "list") {
+		let pm_ctx = pm::PortMidi::new().unwrap();
+		let devices = pm_ctx.devices().unwrap();
+		devices.iter().for_each(|d| println!("[MAIN]: device {} {:?} {:?}", d.id(), d.name(), d.direction()));
+		std::process::exit(0);
+	}
 	
 	let pm_context = pm::PortMidi::new().unwrap();
-	let xone_id = get_xonek2_id(&pm_context);
+	let dev_id = if std::env::args().skip(1).any(|a| a == "Through") { 
+		get_midithroughport_id(&pm_context)  
+	} else if std::env::args().skip(1).any(|a| a == "wine") {
+		get_wine_alsa_input_id(&pm_context)
+	} else { get_xonek2_id(&pm_context) };
 	// get the device info for the given id
-	let info = pm_context.device(xone_id).unwrap();
+	let info = pm_context.device(dev_id).unwrap();
 
-	println!("Listening on: {}) {}", info.id(), info.name());
-	println!("found xone {}", xone_id);
+	println!("Listening on: [{}] => [{}]", info.id(), info.name());
+	println!("found dev_id [{}]", dev_id);
 
 	thread::spawn(move || {
 		// get the device's input port
